092f3cb7cce13c110848e6b4ff3e2fb1005c3e40
ShaliAndrew <116110771+ShaliAndrew@users.noreply.github.com>
2023-04-29 11:05:38 +0200

    Add files via upload

---
+++ UserAccount/src/main/java/com/example/useraccount/mainUserController.java
@@ -0,0 +1,108 @@
+package com.example.useraccount;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class mainUserController {
+    private Stage stage;
+    private Scene scene;
+    private FXMLLoader fxmlLoader;
+    @FXML
+    private Button exitButtonMain;
+    @FXML
+    private Button exitButtonBalance;
+    @FXML
+    private Button exitButtonTransaction;
+
+
+
+    public void switchToMainUser(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainUser.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void switchToViewBalance(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("viewBalance.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+    public void switchToViewTransaction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("viewTransaction.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToMainUser2(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainUser.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToUserInfo(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("viewUserInfo.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToMainUser3(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainUser.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToViewLoanTransaction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("viewLoan.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public void switchToViewTransaction2(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("viewTransaction.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void exitButtonMain(ActionEvent event){
+        System.exit(0);
+
+    }
+    public void exitButtonBalance(ActionEvent event){
+        System.exit(0);
+    }
+    public void exitButtonTransaction(ActionEvent event){
+        System.exit(0);
+    }
+    public void exitLoanButton(ActionEvent event){
+        System.exit(0);
+    }
+    public void exitProfileButton(ActionEvent event){
+        System.exit(0);
+    }
+
+
+}
