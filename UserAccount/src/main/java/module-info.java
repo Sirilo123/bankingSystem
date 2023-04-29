24c650e2ebd27e217cfa2798ff1357dcc90d85c0
ShaliAndrew <116110771+ShaliAndrew@users.noreply.github.com>
2023-04-29 12:26:38 +0200

    Delete UserAccount directory

--- UserAccount/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.useraccount {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.useraccount to javafx.fxml;
-    exports com.example.useraccount;
-}
